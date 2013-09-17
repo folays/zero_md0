66b08bbf0cc21ca0b229eb651703f228c4c9dd7e
Nicolas Limage <nlimage@online.net>
2013-09-17 17:11:28 +0200

    divided benchmarks into multiple runs, fixes, improvements

--- zero_md0.c
+++ zero_md0.c
@@ -50,18 +50,19 @@
  */
 
 static char *mydev = 0; /* device to be used */
-static int mode_write = 0; /* 0 => read, 1 => write */
-static int mode_rnd = 0; /* 0 => sequential, 1 => random */
+static unsigned int mode_write = 0; /* 0 => read, 1 => write */
+static unsigned int mode_rnd = 0; /* 0 => sequential, 1 => random */
 static unsigned long long int mydevsize; /* size of the block device in bytes */
-static int myiosize = 0; /* optimal_io_size */
-static unsigned long long int mycount = 10000; /* number of i/o to do */
-static int copied; /* # of aios dones */
-static int inflight; /* # of aios in flight */
-static int maxinflight = 200; /* maximum # of aios in flight */
-static int maxsubmit = 10; /* maximum # of aios to submit */
+static int myiosize = 0; /* io size */
+static unsigned int mycount = 1000; /* number of i/o to do */
+static unsigned int maxinflight = 200; /* maximum # of aios in flight */
+static unsigned int maxsubmit = 20; /* maximum # of aios to submit */
+static unsigned int myruns = 10; /* number of runs */
+static unsigned int verbose = 0; /* verbose */
 
-struct timeval tv_temp;
-static int copied_temp = 0;
+static unsigned int runid; /* # of aios dones */
+static unsigned int copied; /* # of aios dones */
+static unsigned int inflight; /* # of aios in flight */
 
 /* Fatal error handler */
 static void io_error(const char *func, int rc)
@@ -81,9 +82,6 @@ static void io_error(const char *func, int rc)
 
 static void io_done(io_context_t ctx, struct iocb *iocb, long res, long res2)
 {
-  struct timeval tv_now;
-  double elapsed, avgbw;
-
   if (res2 != 0)
     io_error("aio ", res2);
 
@@ -97,16 +95,8 @@ static void io_done(io_context_t ctx, struct iocb *iocb, long res, long res2)
   ++copied;
   free(iocb);
 
-  /* stats */
-  gettimeofday(&tv_now, NULL);
-  elapsed = (tv_now.tv_sec - tv_temp.tv_sec) * 1000 + (tv_now.tv_usec - tv_temp.tv_usec) / 1000;
-  avgbw = (double)myiosize * copied / elapsed * 1000 / (1024 * 1024);
-  printf("debug: ms=%lfms bw=%lf\n", elapsed, avgbw);
-  /* memcpy(&tv_temp, &tv_now, sizeof(tv_temp));
-  copied_temp = copied; */
-
-  if (copied % 100 == 0)
-    printf("done copy: inflight [%d/%d] copied [%d/%llu]\n",
+  if (verbose && copied % 100 == 0)
+    printf("done copy: inflight [%u/%u] copied [%u/%u]\n",
       inflight, maxinflight, copied, mycount);
 
   if (inflight == 0 && copied < mycount)
@@ -115,8 +105,8 @@ static void io_done(io_context_t ctx, struct iocb *iocb, long res, long res2)
 
 void loop_aio(int fd, void *buf)
 {
-  struct timeval tv_start, tv_end;
   int res;
+  unsigned int elapsed;
 
   /* i/o context initialization */
   io_context_t myctx;
@@ -124,8 +114,10 @@ void loop_aio(int fd, void *buf)
   if ((res = io_queue_init(maxinflight, &myctx)))
     io_error("io_queue_init", res);
 
-  gettimeofday(&tv_start, NULL);
-  memcpy(&tv_temp, &tv_start, sizeof(tv_temp));
+  copied = 0;
+  inflight = 0;
+
+  printf("[run %d] start\n", runid);
 
   while (copied < mycount)
   {
@@ -134,7 +126,6 @@ void loop_aio(int fd, void *buf)
     unsigned long long int index;
     struct iocb *iocb;
     struct timeval tv1, tv2;
-    int elapsed;
 
     /* filling a context with io queries */
     while (copied + inflight + tosubmit < mycount &&
@@ -142,8 +133,7 @@ void loop_aio(int fd, void *buf)
       tosubmit < maxsubmit)
     {
       /* Simultaneous asynchronous operations using the same iocb produce undefined results. */
-      /*iocb = calloc(1, sizeof(*iocb));*/
-      iocb = malloc(sizeof(struct iocb));
+      iocb = calloc(1, sizeof(struct iocb));
 
       if (mode_rnd)
         index = (mydevsize / myiosize) * random() / RAND_MAX;
@@ -195,11 +185,17 @@ void loop_aio(int fd, void *buf)
     }
   }
 
-  gettimeofday(&tv_end, NULL);
+  /*gettimeofday(&tv_end, NULL);*/
   io_queue_release(myctx);
 
   /* calculating statistics */
+  /*
   int elapsed = (tv_end.tv_sec - tv_start.tv_sec) * 1000 + (tv_end.tv_usec - tv_start.tv_usec) / 1000;
+
+  stat_time += elapsed;
+  */
+
+/*
   printf("all job done in %d ms\n", elapsed);
   printf("iop/s: %d\n", (int)((double)mycount / elapsed * 1000));
   printf("average lantency in ms: %.3f\n", (double)elapsed / mycount);
@@ -207,16 +203,19 @@ void loop_aio(int fd, void *buf)
     (double)myiosize * mycount / elapsed * 1000 / (1024 * 1024),
     (double)myiosize * mycount / elapsed * 1000 / (1024 * 1024) * 8);
   printf("total size in MB: %lld\n", (unsigned long long)myiosize * mycount / 1024 / 1024);
+  */
 }
 
 static struct option long_options[] = {
   {"dev", required_argument, NULL, 'd'},
   {"iosize", required_argument, NULL, 's'},
   {"write", no_argument, NULL, 'w'},
-  {"iocount", required_argument, NULL, 'c'},
   {"random", no_argument, NULL, 'r'},
+  {"iocount", required_argument, NULL, 'c'},
   {"maxsubmit", required_argument, NULL, 'b'},
   {"maxinflight", required_argument, NULL, 'f'},
+  {"runs", required_argument, NULL, 'n'},
+  {"verbose", no_argument, NULL, 'v'},
   {NULL, 0, NULL, 0},
 };
 
@@ -270,6 +269,17 @@ int main_getopt(int argc, char **argv)
           return 1;
         }
         break;
+      case 'v':
+        verbose = 1;
+        break;
+      case 'n':
+        myruns = atoi(optarg);
+        if (myruns == 0)
+        {
+          fprintf(stderr, "Invalid maximum runs: %s\n", optarg);
+          return 1;
+        }
+        break;
     }
   }
   return 0;
@@ -277,13 +287,19 @@ int main_getopt(int argc, char **argv)
 
 int usage(char *s)
 {
-  fprintf(stderr, "Usage: %s <--dev=/dev/xxx> [--write] [--iosize=x (kB)] [--iocount=x]\
-[--random] [--maxsubmit=x] [--maxinflight=x]\n", s);
+  fprintf(stderr, "Usage: %s <--dev=/dev/xxx> [--write] [--random] \
+[--iosize=x (kB)] [--iocount=x] [--runs=x] [--verbose]\
+[--maxsubmit=x] [--maxinflight=x]\n", s);
   return 2;
 }
 
 int main(int argc, char **argv)
 {
+  struct timeval tv_start, tv_end;
+  unsigned int elapsed;
+  unsigned int stat_time = 0;
+  unsigned int stat_iocount = 0;
+
   if (main_getopt(argc, argv))
     return 1;
 
@@ -296,10 +312,8 @@ int main(int argc, char **argv)
   CPU_SET(0, &mask);
   sched_setaffinity(0, sizeof(mask), &mask);
 
-  printf("Using device: %s\n", mydev);
-
-  //printf("sizeof 8 bytes type : %lu\n", sizeof(long long int));
   /* opening device */
+  printf("Using device: %s\n", mydev);
   int fd = open(mydev, O_RDWR | O_DIRECT);
   if (fd < 0)
     err(1, "open %s", mydev);
@@ -322,10 +336,13 @@ int main(int argc, char **argv)
       printf("Using auto-detected optimal_io_size for %s\n", mydev);
   }
 
-  printf("Performing %s %s test\n", mode_rnd ? "random " : "sequential",  mode_write ? "write" : "read");
-  printf("Will perform %llu x %lukB IOs\n", mycount, (long unsigned int)(myiosize / 1024));
-  printf("Maximum IO submit / inflight: %d / %d\n", maxsubmit, maxinflight);
-  //mycount = mydevsize / myiosize;
+  printf("Performing %s %s test, %d runs\n",
+    mode_rnd ? "random " : "sequential",
+    mode_write ? "write" : "read", myruns);
+  printf("Will perform %u x %ukB IOs\n",
+    mycount, myiosize / 1024);
+  printf("Maximum IO submit / inflight: %d / %d\n",
+    maxsubmit, maxinflight);
 
   /* advising kernel of future io pattern */
   if (mode_rnd)
@@ -343,7 +360,36 @@ int main(int argc, char **argv)
   for (i = 0; i < myiosize / sizeof(int); ++i)
     ((int *)buf)[i] = rand();
 
-  loop_aio(fd, buf);
+  /* start X runs and collect stats */
+  for (runid = 0; runid < myruns; ++runid)
+  {
+    gettimeofday(&tv_start, NULL);
+    loop_aio(fd, buf);
+    gettimeofday(&tv_end, NULL);
+
+    /* update stats */
+    elapsed = (tv_end.tv_sec - tv_start.tv_sec) * 1000 + (tv_end.tv_usec - tv_start.tv_usec) / 1000;
+    stat_time += elapsed;
+    stat_iocount += mycount;
+
+    if (verbose)
+    {
+      printf("[run %d] done in %d ms\n", runid, stat_time);
+      printf("[run %d] iop/s: %d\n", runid,
+        (unsigned int)((double)mycount / elapsed * 1000));
+      printf("[run %d] avg latency (ms): %.3lf\n",
+        runid, (double)elapsed / mycount);
+    }
+  }
+
+  printf("%d io done in %d ms\n", stat_iocount, stat_time);
+  printf("average iop/s: %.2lf\n", (double)stat_iocount / stat_time * 1000);
+  printf("average latency: %.3f ms\n", (double)stat_time / stat_iocount);
+  printf("average bandwidth: %.3f MB/s (%.3f Mb/s)\n",
+    (double)myiosize * stat_iocount / stat_time * 1000 / (1024 * 1024),
+    (double)myiosize * stat_iocount / stat_time * 1000 / (1024 * 1024) * 8);
+  printf("total data size: %lld MB\n",
+    (unsigned long long)myiosize * stat_iocount / 1024 / 1024);
 
   return 0;
 }
